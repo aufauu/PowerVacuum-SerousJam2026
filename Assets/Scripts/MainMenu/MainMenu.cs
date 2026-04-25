74470f5de6f0cd01434b86ade9fd06bf8e01b847
aufauu <zuzanna.sowul@gmail.com>
2026-04-25 14:57:14 +0200

    fonts

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("LayOut");
+    }
+      public void Quit()
+    {
+        Application.Quit;
+    }
+}
